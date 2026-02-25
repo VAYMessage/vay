0b70215cf3939c2229a2d2ed5d6e4aa2e76d59b5
VaySafetyInternet <texpoderjka_vaydesigning@bk.ru>
2026-02-25 22:55:12 +0300

    Update router.js

--- js/core/router.js
+++ js/core/router.js
@@ -1,9 +1,7 @@
-export function checkAccess(user, roleRequired) {
-  if (!user) {
-    window.location.href = "/403.html";
-  }
+export function requireAuth(user) {
+  if (!user) window.location.href = "/";
+}
 
-  if (roleRequired && user.role !== roleRequired) {
-    window.location.href = "/403.html";
-  }
+export function requireAdmin(isAdmin) {
+  if (!isAdmin) window.location.href = "/403.html";
 }
