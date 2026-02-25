355b0d1c2aaba178f1c2b180b8d16a3084831628
VaySafetyInternet <texpoderjka_vaydesigning@bk.ru>
2026-02-25 22:45:41 +0300

    Delete admin.js

--- admin.js
+++
@@ -1,7 +0,0 @@
-import { httpsCallable } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-functions.js";
-import { functions } from "./firebase.js";
-
-export async function sendByRole(role, message) {
-  const fn = httpsCallable(functions, "sendNotificationByRole");
-  return await fn({ role, message });
-}
